8d0b5e4a1711db6a1c0d2ecef22b258251357c9f
Tamás <icyplusplus@gmail.com>
2017-10-20 19:32:08 +0200

    03_cube.

--- README.md
+++ README.md
@@ -2,11 +2,17 @@ Oglwrap examples
 ================
 
 [01_square.cpp](src/cpp/01_square.cpp)
--------------------------------------- 
+--------------------------------------
 
-![01_square](screenshots/01_square.jpg) 
+![01_square](screenshots/01_square.jpg)
 
 [02_textured_square.cpp](src/cpp/02_textured_square.cpp)
--------------------------------------- 
+--------------------------------------
 
 ![02_textured_square](screenshots/02_textured_square.jpg)
+
+[03_cube.cpp](src/cpp/03_cube.cpp)
+--------------------------------------
+
+![03_cube](screenshots/03_cube.jpg)
+
---
+++ src/cpp/03_cube.cpp
@@ -0,0 +1,90 @@
+// Copyright (c), Tamas Csala
+
+#include "oglwrap_example.hpp"
+
+#include <lodepng.h>
+#include <oglwrap/oglwrap.h>
+#include <oglwrap/shapes/cube_shape.h>
+#include <glm/gtc/matrix_transform.hpp>
+
+class CubeExample : public OglwrapExample {
+private:
+  // Defines a full screen rectangle (see oglwrap/shapes/rectangle.h)
+  gl::CubeShape cube_shape_;
+
+  // A shader program
+  gl::Program prog_;
+
+  // A 2D texture
+  gl::Texture2D tex_;
+
+public:
+  CubeExample ()
+    // Now we need texture coordinates too, not just position
+    : cube_shape_({gl::CubeShape::kPosition,
+                   gl::CubeShape::kNormal})
+  {
+    // We need to add a few more lines to the shaders
+    gl::ShaderSource vs_source;
+    vs_source.set_source(R"""(
+      #version 330 core
+      in vec4 inPos;
+      in vec3 inNormal;
+
+      uniform mat4 mvp;
+
+      out vec3 normal;
+
+      void main() {
+        normal = inNormal;
+        gl_Position = mvp * inPos;
+      })""");
+    vs_source.set_source_file("example_shader.vert");
+    gl::Shader vs(gl::kVertexShader, vs_source);
+
+    gl::ShaderSource fs_source;
+    fs_source.set_source(R"""(
+      #version 330 core
+      in vec3 normal;
+
+      out vec4 fragColor;
+
+      void main() {
+        vec3 lightPos = normalize(vec3(0.3, 1, 0.2));
+        float diffuseLighting = max(dot(lightPos, normalize(normal)), 0.0);
+        vec3 color = vec3(0.1, 0.3, 0.8);
+        fragColor = vec4(diffuseLighting * color, 1.0);
+      })""");
+    fs_source.set_source_file("example_shader.frag");
+    gl::Shader fs(gl::kFragmentShader, fs_source);
+
+    // Create a shader program
+    prog_.attachShader(vs);
+    prog_.attachShader(fs);
+    prog_.link();
+    gl::Use(prog_);
+
+    // Shader plumbing
+    (prog_ | "inPos").bindLocation(gl::CubeShape::kPosition);
+    (prog_ | "inNormal").bindLocation(gl::CubeShape::kNormal);
+
+    gl::Enable(gl::kDepthTest);
+
+    // Set the clear color
+    gl::ClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+  }
+
+protected:
+  virtual void Render() override {
+    float t = glfwGetTime();
+    glm::mat4 camera_mat = glm::lookAt(1.5f*glm::vec3{sin(t), 1.0f, cos(t)}, glm::vec3{0.0f, 0.0f, 0.0f}, glm::vec3{0.0f, 1.0f, 0.0f});
+    glm::mat4 proj_mat = glm::perspectiveFov<float>(M_PI/3.0, 600, 600, 0.1, 100);
+    gl::Uniform<glm::mat4>(prog_, "mvp") = proj_mat * camera_mat;
+    cube_shape_.render();
+  }
+};
+
+int main() {
+  CubeExample().RunMainLoop();
+}
+
